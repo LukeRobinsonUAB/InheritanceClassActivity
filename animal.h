848a799916facc71d7f020981c33ce181d1f0b52
Luke Robinson <ldrobins@uab.edu>
2024-09-26 10:26:03 -0500

    main

--- animal.h
+++ animal.h
@@ -5,31 +5,15 @@
 #include <iostream>
 class Animal {
     public:
-        Animal() {
-            int age = 0;
-            std::string name = "No name";
-            std::string type = "Animal";
+        Animal();
 
-        }
+        Animal(int age2, std::string name2, std::string type2);
 
-        Animal (int age2, std::string name2, std::string type2) {
-            int age = age2;
-            std::string name = name2;
-            std::string type = type2;
-        }
+        std::string getType();
 
-        std::string getType() {
-            return this->type;
-        }
+        std::string getName();
 
-        std::string getName() {
-            return this->name;
-        }
-
-        void displayInfo () {
-            std::cout << "age: " << this->age << " name: " << this->name << " Type: " << this->type << std::endl;
-        }
-
+        void displayInfo ();
 
     private:
         int age;
@@ -40,26 +24,11 @@ class Animal {
 
 class Dog : public Animal {
     public:
-        Dog() {
-            int age = 0;
-            std::string name = "No name";
-            std::string type = "Animal";
-            std::string breed = "unknown";
+        Dog();
 
-        }
+        Dog (int age2, std::string name2, std::string type2, std::string breed2);
 
-        Dog (int age2, std::string name2, std::string type2, std::string breed2) {
-            int age = age2;
-            std::string name = name2;
-            std::string type = type2;
-            std::string breed = breed2;
-        }
-
-        void displayInfo () {
-            std::cout << "age: " << this->age << " name: " << this->name << " Type: " << this->type <<  " breed: " << this->breed << std::endl;
-        }
-
-
+        void displayInfo();
 
     private:
         int age;
@@ -71,25 +40,11 @@ class Dog : public Animal {
 
 class Cat : public Animal {
     public:
-        Cat() {
-            int age = 0;
-            std::string name = "No name";
-            std::string type = "Animal";
-            std::string breed = "unknown";
+        Cat();
 
-        }
+        Cat (int age2, std::string name2, std::string type2, std::string breed2);
 
-        Cat (int age2, std::string name2, std::string type2, std::string breed2) {
-            int age = age2;
-            std::string name = name2;
-            std::string type = type2;
-            std::string breed = breed2;
-        }
-
-        void displayInfo () {
-            std::cout << "age: " << this->age << " name: " << this->name << " Type: " << this->type <<  " breed: " << this->breed << std::endl;
-        }
-
+        void displayInfo ();
 
 
     private:
